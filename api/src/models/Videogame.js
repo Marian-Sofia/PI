5be899c4ae72271f233afe6e0bcef87eae6469f0
Marian-Sofia <sofiagutirrez0031@gmal.com>
2023-05-08 18:42:32 -0500

    PI

---
+++ api/src/models/Videogame.js
@@ -0,0 +1,38 @@
+const { DataTypes } = require ('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('videogame', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    decription: {
+      type: DataTypes.STRING
+    },
+    platform: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    date: {
+      type: DataTypes.DATEONLY,
+      allowNull: false
+    },
+    rating: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
+};
